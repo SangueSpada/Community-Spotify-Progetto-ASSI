35b4bd53026d50879944fab317cd888bac299a7f
ValerioLorito <lorito.1885657@studenti.uniroma1.it>
2022-07-03 19:45:55 +0200

    MODELLI DA MODIFICARE, NON FARE ULTERIORI MODIFICHE

--- app/controllers/community_posts_controller.rb
+++ app/controllers/community_posts_controller.rb
@@ -1,4 +1,4 @@
-class CommunityPostsController < ApplicationController
+class CommunityPostsController < PostsController
     skip_before_action :verify_authenticity_token
 
   def show
--- app/models/community_post.rb
+++ app/models/community_post.rb
@@ -1,4 +1,4 @@
-class CommunityPost < ApplicationRecord
+class CommunityPost < Post
     belongs_to :community
 
     has_many :comments, dependent: :destroy
