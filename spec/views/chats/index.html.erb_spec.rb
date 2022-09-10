c0f04e00c81e0086057601e75f42483bb4fd16a7
Daniele Corvari <corvari.1883299@studenti.uniroma1.it>
2022-09-10 14:30:05 +0200

    aggiunti test di acceptance su chat e su community_reccomendations

--- spec/views/chats/index.html.erb_spec.rb
+++ spec/views/chats/index.html.erb_spec.rb
@@ -1,15 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "chats/index", type: :view do
-=begin   before(:each) do
-    assign(:chats, [
-      Chat.create!(),
-      Chat.create!()
+  user1 = User.new(email: "daniele@example.com",password: "password",id:1,uid:"1", name:"daniele" );
+  user2 = User.new(email: "fabio@example.com",password: "password",id:2,uid:"2" , name: "fabio");
+  user3 = User.new(email: "fabio@example.com",password: "password",id:3,uid:"3", name: "fabio" );
+=begin   before do
+    sign_in user1
+  end 
+=end
+  before(:each) do
+    @chats=assign(:chats, [
+      Chat.create!(user1: user1,user2: user2),
+      Chat.create!(user1: user3,user2: user1),
+      Chat.create!(user1: user3,user2: user2)
     ])
-  end
-
+  end    
   it "renders a list of chats" do
     render
+    assert_select 'li>div>span', text: 'fabio'.to_s,count:2
+    assert_select 'li>div>span', text: 'fabio'.to_s,count:2
   end
-=end
 end
